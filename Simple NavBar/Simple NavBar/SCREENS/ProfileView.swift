fa424dc577bdb4dcec7c32c5b898e36d11ae6eb8
Supervue MM 1 <supervuemm1@Supervues-Mini.localdomain>
2026-05-12 15:59:50 +0530

    A Simple navigation Bar in a structured way

---
+++ Simple NavBar/Simple NavBar/SCREENS/ProfileView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileView.swift
+//  Simple NavBar
+//
+//  Created by Supervue MM 1 on 12/05/26.
+//
+
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
